111aad5e76262803735e2809c0577008997eae07
Isaque Hernandes <isaquehernandesfs@gmail.com>
2023-12-03 00:05:54 -0300

    Separate nanostore storage key and cache storage key

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,7 +1,7 @@
 import type { Theme } from "@/types/Theme";
 import { persistentAtom } from "@nanostores/persistent";
 
-export const $theme = persistentAtom<Theme>("theme", "light");
+export const $theme = persistentAtom<Theme>("persisted_theme", "light");
 
 export const setTheme = (theme: Theme) => {
   $theme.set(theme);
