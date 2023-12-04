2724005318a0f913fb6446c9933f0558b8b9ee6f
Isaque Hernandes <isaquehernandesfs@gmail.com>
2023-12-04 13:11:07 -0300

    Change cloudflare to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
